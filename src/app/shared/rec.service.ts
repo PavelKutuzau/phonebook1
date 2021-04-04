563996e1571d646544eb57aa0e833a203f4dcafe
PavelKutuzau <pavelkutuzau@gmail.com>
2021-04-04 19:40:53 +0300

    Create service for records Displaying the array of co-workers on the screen

--- src/app/shared/rec.service.ts
+++ src/app/shared/rec.service.ts
@@ -2,17 +2,22 @@ import {Injectable} from '@angular/core';
 
 export interface Rec {
   id: number;
-  depP: string;    // department parent
+  depP?: string;    // department parent
   dep: string;     // department
   name: string;
-  phone: string;   // phone mobile
+  phone?: string;   // phone mobile
   phoneW: string;  // phone work
-  phoneWM: string; // phone mobile
+  phoneWM?: string; // phone mobile
 }
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class RecService {
-  public recs: Rec[] = []; // records
+  // records
+  public recs: Rec[] = [
+    {id: 1, dep: 'Администрация', name: 'Иванов Иван Иванович', phoneW: '1-23'},
+    {id: 2, dep: 'Бухгалтерия', name: 'Петров Петр Петрович', phoneW: '2-34'},
+    {id: 3, dep: 'Склад', name: 'Сидорова Анжелина Сидоровна', phoneW: '3-45'}
+  ];
 
   constructor() {
   }
